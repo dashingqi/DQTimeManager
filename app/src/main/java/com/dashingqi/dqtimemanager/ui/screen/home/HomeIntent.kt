75f19fe48f21162b0df91e5c3e64833df482cb15
zhangqi61 <zhangqi61@baidu.com>
2023-08-29 13:40:05 +0800

    【update】

---
+++ app/src/main/java/com/dashingqi/dqtimemanager/ui/screen/home/HomeIntent.kt
@@ -0,0 +1,11 @@
+package com.dashingqi.dqtimemanager.ui.screen.home
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author zhangqi61
+ * @since 2023/8/28
+ */
+class HomeViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/dashingqi/dqtimemanager/ui/screen/home/HomePageState.kt
@@ -0,0 +1,8 @@
+package com.dashingqi.dqtimemanager.ui.screen.home
+
+/**
+ * home-state
+ * @author zhangqi61
+ * @since 2023/8/28
+ */
+
